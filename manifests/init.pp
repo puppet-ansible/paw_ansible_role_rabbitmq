03640503d6bc3e613ae5805ca41c7be203f8adcc
Chris Allen <chris.j.allen1979@gmail.com>
2026-02-04 12:48:32 +0000

    Automated conversion from geerlingguy.rabbitmq Ansible role

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,38 +52,38 @@ class paw_ansible_role_rabbitmq (
 # Execute the Ansible role using PAR (Puppet Ansible Runner)
 # Playbook synced via pluginsync to agent's cache directory
 # Check for common paw::par_vardir setting, then module-specific, then default
-  $_par_vardir = $par_vardir ? {
-    undef   => lookup('paw::par_vardir', Stdlib::Absolutepath, 'first', '/opt/puppetlabs/puppet/cache'),
-    default => $par_vardir,
-  }
-  $playbook_path = "${_par_vardir}/lib/puppet_x/ansible_modules/ansible_role_rabbitmq/playbook.yml"
+$_par_vardir = $par_vardir ? {
+  undef   => lookup('paw::par_vardir', Stdlib::Absolutepath, 'first', '/opt/puppetlabs/puppet/cache'),
+  default => $par_vardir,
+}
+$playbook_path = "${_par_vardir}/lib/puppet_x/ansible_modules/ansible_role_rabbitmq/playbook.yml"
 
-  par { 'paw_ansible_role_rabbitmq-main':
-    ensure        => present,
-    playbook      => $playbook_path,
-    playbook_vars => {
-      'rabbitmq_daemon'         => $rabbitmq_daemon,
-      'rabbitmq_state'          => $rabbitmq_state,
-      'rabbitmq_enabled'        => $rabbitmq_enabled,
-      'rabbitmq_version'        => $rabbitmq_version,
-      'rabbitmq_rpm'            => $rabbitmq_rpm,
-      'rabbitmq_rpm_url'        => $rabbitmq_rpm_url,
-      'rabbitmq_rpm_gpg_url'    => $rabbitmq_rpm_gpg_url,
-      'rabbitmq_apt_repository' => $rabbitmq_apt_repository,
-      'rabbitmq_apt_gpg_url'    => $rabbitmq_apt_gpg_url,
-      'erlang_apt_repository'   => $erlang_apt_repository,
-      'erlang_apt_gpg_url'      => $erlang_apt_gpg_url,
-    },
-    tags          => $par_tags,
-    skip_tags     => $par_skip_tags,
-    start_at_task => $par_start_at_task,
-    limit         => $par_limit,
-    verbose       => $par_verbose,
-    check_mode    => $par_check_mode,
-    timeout       => $par_timeout,
-    user          => $par_user,
-    env_vars      => $par_env_vars,
-    logoutput     => $par_logoutput,
-    exclusive     => $par_exclusive,
-  }
+par { 'paw_ansible_role_rabbitmq-main':
+  ensure        => present,
+  playbook      => $playbook_path,
+  playbook_vars => {
+        'rabbitmq_daemon' => $rabbitmq_daemon,
+        'rabbitmq_state' => $rabbitmq_state,
+        'rabbitmq_enabled' => $rabbitmq_enabled,
+        'rabbitmq_version' => $rabbitmq_version,
+        'rabbitmq_rpm' => $rabbitmq_rpm,
+        'rabbitmq_rpm_url' => $rabbitmq_rpm_url,
+        'rabbitmq_rpm_gpg_url' => $rabbitmq_rpm_gpg_url,
+        'rabbitmq_apt_repository' => $rabbitmq_apt_repository,
+        'rabbitmq_apt_gpg_url' => $rabbitmq_apt_gpg_url,
+        'erlang_apt_repository' => $erlang_apt_repository,
+        'erlang_apt_gpg_url' => $erlang_apt_gpg_url
+              },
+  tags          => $par_tags,
+  skip_tags     => $par_skip_tags,
+  start_at_task => $par_start_at_task,
+  limit         => $par_limit,
+  verbose       => $par_verbose,
+  check_mode    => $par_check_mode,
+  timeout       => $par_timeout,
+  user          => $par_user,
+  env_vars      => $par_env_vars,
+  logoutput     => $par_logoutput,
+  exclusive     => $par_exclusive,
+}
 }
